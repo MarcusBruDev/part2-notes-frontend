e107fca00e31cc2ee20009ea3cfaa583ea1ae003
Marcus Bru <marcusbrudev@gmail.com>
2024-12-05 10:30:04 -0500

    Agregur formulario para que el usuario agregue mas notas, filtras por notas importante y boton mostrar notas importante y todas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,52 @@
 import Note from './components/Note'
+import { useState } from 'react'
 
 //comentario
-const App = ({ notes }) => {
+const App = (props) => {
+
+  
+  const[notes,setNotesState]= useState(props.notes);
+  const[newNote,setNewNote]= useState('a new note...')
+  const[showAll,setShowAll]= useState(true)
+  
+
+  const addNote = (event)=>{
+    event.preventDefault();
+    
+    const newObject = {
+      content: newNote,
+      important : Math.random()< 0.5,
+      id: notes.length +1
+    }
+
+    setNotesState(notes.concat(newObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event)=>{
+    
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow= showAll ? notes : notes.filter(note =>note.important)
+
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={()=>{setShowAll(!showAll)}}>Show {showAll ? 'Important' : 'All'}</button>
+      </div>
+
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
           <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type='submit' >Save</button>
+      </form>
     </div>
   )
 }
